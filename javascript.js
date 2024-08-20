3eba8c07149adceafca9c3d788e84b25c30533cd
Adnan Semic <148137889+ShemichAdnan@users.noreply.github.com>
2024-08-20 12:42:54 +0200

    Zavrsen human choice

--- javascript.js
+++ javascript.js
@@ -8,5 +8,16 @@ let getComputerChoice=()=>{
         return "scissors"
     }
 }
-console.log(getComputerChoice())
+let getHumanChoice=()=>{
+    let choice
+    do{
+        choice=prompt("Unesite izbor (rock,paper,scissors): ")
+        if(choice !="rock" && choice !="paper" && choice !="scissors"){
+            alert("[GRESKA] Unijeli ste pogresan izbor!")
+            console.log("[GRESKA] Unijeli ste pogresan izbor!")
+        }
+    }while(choice !="rock" && choice !="paper" && choice !="scissors")
+        return choice;
+}
+console.log(getHumanChoice())
 
