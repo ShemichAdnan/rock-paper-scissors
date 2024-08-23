b1cc66cf63813e4f9be6ef6a6d39213665446b52
Adnan Semic <148137889+ShemichAdnan@users.noreply.github.com>
2024-08-24 00:30:55 +0200

    Zavrsen HTML i CSS sa nekim funkcijama u js

--- javascript.js
+++ javascript.js
@@ -8,63 +8,64 @@ let getComputerChoice=()=>{
         return "scissors"
     }
 }
-let getHumanChoice=()=>{
-    let choice
-    do{
-        choice=prompt("Write your choice (rock,paper,scissors): ")
-        if(choice!=null){
-            choice=choice.toLowerCase();
-            if(choice !="rock" && choice !="paper" && choice !="scissors"){
-                alert("[GRESKA] Unijeli ste pogresan izbor!")
-                console.log("[GRESKA] Unijeli ste pogresan izbor!")
-            }
-        }
-    }while(choice !="rock" && choice !="paper" && choice !="scissors")
-        return choice;
-}
+
 
 let humanScore=0
 let computerScore=0
+const announce=document.querySelector("#announce");
+const hScore=document.querySelector("#hScore");
+const cScore=document.querySelector("#cScore")
 
 function playRound(humanChoice, computerChoice) {
     console.log(`${humanChoice} vs ${computerChoice}`)
     if(humanChoice=="rock"){
         if(computerChoice=="rock") {
-            console.log("Draw! You both chose Rock")
+            announce.textContent="Tie! You both chose Rock";
+            announce.style.color="gray";
             return
         }else if(computerChoice=="paper"){
-            console.log("You lose! Paper beats Rock")
+            announce.textContent="You lose! Paper beats Rock";
+            announce.style.color="red";
             computerScore++;
+            cScore.textContent=computerScore;
             return 
         }else if(computerChoice=="scissors"){
-            console.log("You won! Rock beats Scissors")
+            announce.textContent="You won! Rock beats Scissors";
+            announce.style.color="green";
             humanScore++;
+            hScore.textContent=humanScore;
             return 
         }
     }else if(humanChoice=="paper"){
         if(computerChoice=="rock") {
             console.log("You won! Paper beats Rock")
+            announce.style.color="green";
             humanScore++;
+            hScore.textContent=humanScore;
             return
         }else if(computerChoice=="paper"){
-            console.log("Draw! You both chose Paper")
+            console.log("Tie! You both chose Paper")
             return 
         }else if(computerChoice=="scissors"){
             console.log("You lose! Scissors beats Paper")
             computerScore++;
+            cScore.textContent=computerScore;
             return 
         }
     }else if(humanChoice=="scissors"){
         if(computerChoice=="rock") {
             console.log("You lose! Rock beats Scissors")
             computerScore++;
+            cScore.textContent=computerScore;
             return
         }else if(computerChoice=="paper"){
             console.log("You won! Scissors beats Paper")
+            announce.style.color="green";
             humanScore++;
+            hScore.textContent=humanScore;
             return 
         }else if(computerChoice=="scissors"){
-            console.log("Draw! You both chose Scissors")
+            console.log("Tie! You both chose Scissors")
             return 
         }
     }
@@ -72,14 +73,11 @@ function playRound(humanChoice, computerChoice) {
   
 
   
-let playGame=()=>{
-    for(let round=1;round<=5;round++){
-        console.log(`Round - ${round}`)
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-        console.log(`${humanScore} - ${computerScore}`)
-    }
+/*let playGame=()=>{
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    console.log(`${humanScore} - ${computerScore}`)
+    
     if(humanScore>computerScore){
         console.log(`You won the game! [${humanScore} - ${computerScore}]`)
     }else if(humanScore<computerScore){
@@ -90,4 +88,16 @@ let playGame=()=>{
     computerScore=0
     humanScore=0
     alert("Thanks for playing!")
-}
+}*/
+const rock=document.querySelector("#rock");
+const paper=document.querySelector("#paper");
+const scissors=document.querySelector("#scissors");
+rock.addEventListener("click",()=>{
+    playRound("rock",getComputerChoice())
+})
+paper.addEventListener("click",()=>{
+    playRound("paper",getComputerChoice())
+})
+scissors.addEventListener("click",()=>{
+    playRound("scissors",getComputerChoice())
+})
